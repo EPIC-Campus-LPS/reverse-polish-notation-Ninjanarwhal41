2435c35cdf526a6fb667427187d4094ddfd3cbff
Ninjanarwhal41 <elicohrs@gmail.com>
2026-02-11 15:09:25 -0700

    worked on the second method and added a helper method.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(ReversePolishNotation.evaluatePostfix("7 2 + 9 / 3 5 * -"));
         System.out.println(ReversePolishNotation.evaluatePostfix("7 11 + 9 / 3 5 * -"));
-        System.out.println(ReversePolishNotation.evaluatePostfix("7 2 + 9 / 3 5 -"));
+        //System.out.println(ReversePolishNotation.evaluatePostfix("+ * 2 3 - 4 5")); in prefix notation, should throw an error
+        System.out.println(ReversePolishNotation.infixtoPostfix("(7 + 2) / 9 - 3 * 5"));
+        System.out.println(ReversePolishNotation.infixtoPostfix("(6 + 4) - 12 * 7 / (14 - 3)"));
+
     }
 }
--- src/ReversePolishNotation.java
+++ src/ReversePolishNotation.java
@@ -17,13 +17,13 @@ public class ReversePolishNotation {
 
                 switch (arr[i]){
                     case "+":
-                        result = num1 + num2;
+                        result = num2 + num1;
                         break;
                     case "-":
                         result = num2 - num1;
                         break;
                     case "*":
-                        result = num1 * num2;
+                        result = num2 * num1;
                         break;
                     case "/":
                         result = num2 / num1;
@@ -41,6 +41,63 @@ public class ReversePolishNotation {
     }
 
     public static String infixtoPostfix(String input){
-        return null;
+        String output = "";
+        Stack s = new Stack();
+        String[] arr = input.split(" ");
+        for(int i = 0; i < arr.length; i++){
+            if (Character.isDigit(arr[i].charAt(0))){
+                output += arr[i] + " ";
+            }
+            else{
+              if (s.isEmpty()){
+                  s.push(arr[i]);
+                }
+              else{
+                  if (checkPrecedence(arr[i], s.peek()){
+                      s.push(arr[i]);
+                  }
+                  else{
+                      output += s.pop();
+                  }
+              }
+            }
+        }
+
+        return output;
+    }
+    public static boolean checkPrecedence(String a, String b){
+        int order = 0;
+        int order2 = 0;
+        switch (a){
+            case "+", "-":
+                order = 1;
+                break;
+            case "*", "/":
+                order = 2;
+                break;
+            case "^":
+                order = 3;
+                break;
+            case "(":
+                order = 4;
+                break;
+        }
+        switch (b){
+            case "+", "-":
+                order2 = 1;
+                break;
+            case "*", "/":
+                order2 = 2;
+                break;
+            case "^":
+                order2 = 3;
+                break;
+            case "(":
+                order2 = 4;
+                break;
+        }
+        return order > order2;
+        //if the first one has higher precedence, return true
+        //if the second one has higher precedence, return false.
     }
 }
